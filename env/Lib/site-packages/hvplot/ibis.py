0b5cd299eff99fc044a0d6d60faa9cdc76989934
Masuku <sihlemsk2@gmail.com>
2024-07-08 11:30:23 +0200

    commit

---
+++ env/Lib/site-packages/hvplot/ibis.py
@@ -0,0 +1,21 @@
+"""
+Experimental support for ibis.
+"""
+
+
+def patch(name='hvplot', extension='bokeh', logo=False):
+    from . import hvPlotTabular, post_patch
+
+    try:
+        import ibis
+    except ImportError:
+        raise ImportError('Could not patch plotting API onto ibis. Ibis could not be imported.')
+    _patch_plot = lambda self: hvPlotTabular(self)  # noqa: E731
+    _patch_plot.__doc__ = hvPlotTabular.__call__.__doc__
+    patch_property = property(_patch_plot)
+    setattr(ibis.Expr, name, patch_property)
+
+    post_patch(extension, logo)
+
+
+patch()
