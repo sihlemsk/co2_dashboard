eb95d21d7971147f0907dd56cf1b501f852c5219
Masuku <sihlemsk2@gmail.com>
2024-07-08 12:58:33 +0200

    commit

--- dashboard.py
+++ dashboard.py
@@ -3,27 +3,27 @@ import numpy as np
 import panel as pn
 import hvplot.pandas
 
-#Enable Panel extensions
+# Enable Panel extensions
 pn.extension('tabulator')
 
-#Load data
+# Load data
 df = pd.read_csv('https://raw.githubusercontent.com/sihlemsk/co2_dashboard/master/owid-co2-data.csv')
 
-#Data Preprocessing
+# Data Preprocessing
 df = df.fillna(0)
 df['gdp_per_capita'] = np.where(df['population'] != 0, df['gdp'] / df['population'], 0)
 idf = df.interactive()
 
-#Create widgets
+# Create widgets
 year_slider = pn.widgets.IntSlider(name='Year Slider', start=1750, end=2020, step=5, value=1850)
 yaxis_co2 = pn.widgets.RadioButtonGroup(name='Y axis', options=['co2', 'co2_per_capita'], button_type='success')
 yaxis_co2_source = pn.widgets.RadioButtonGroup(name='Y axis', options=['coal_co2', 'oil_co2', 'gas_co2'], button_type='success')
 
-#Define continents list
+# Define continents list
 continents = ['World', 'Asia', 'Oceania', 'Europe', 'Africa', 'North America', 'South America', 'Antarctica']
 continents_excl_world = ['Asia', 'Oceania', 'Europe', 'Africa', 'North America', 'South America', 'Antarctica']
 
-#CO2 emissions by continent pipeline
+# CO2 emissions by continent pipeline
 co2_pipeline = (
     idf[(idf.year <= year_slider) & (idf.country.isin(continents))]
     .groupby(['country', 'year'])[yaxis_co2].mean()
@@ -33,7 +33,7 @@ co2_pipeline = (
     .reset_index(drop=True)
 )
 
-#CO2 emissions vs GDP pipeline
+# CO2 emissions vs GDP pipeline
 co2_vs_gdp_scatterplot_pipeline = (
     idf[(idf.year == year_slider) & (~idf.country.isin(continents))]
     .groupby(['country', 'year', 'gdp_per_capita'])['co2'].mean()
@@ -43,7 +43,7 @@ co2_vs_gdp_scatterplot_pipeline = (
     .reset_index(drop=True)
 )
 
-#CO2 sources by continent pipeline
+# CO2 sources by continent pipeline
 co2_source_bar_pipeline = (
     idf[(idf.year == year_slider) & (idf.country.isin(continents_excl_world))]
     .groupby(['year', 'country'])[yaxis_co2_source].sum()
@@ -53,7 +53,7 @@ co2_source_bar_pipeline = (
     .reset_index(drop=True)
 )
 
-#Plots and tables
+# Plots and tables
 co2_plot = co2_pipeline.hvplot(x='year', by='country', y=yaxis_co2, line_width=2, title="CO2 emission by continent")
 
 co2_vs_gdp_scatterplot = co2_vs_gdp_scatterplot_pipeline.hvplot(
@@ -65,19 +65,19 @@ co2_table = co2_pipeline.pipe(pn.widgets.Tabulator, pagination='remote', page_si
 
 # Dashboard layout
 template = pn.template.FastListTemplate(
-    title = 'World CO2 Emission Dashboard',
-    sidebar = [pn.pane.Markdown('# CO2 Emissions and Climate Change'),
-               pn.pane.Markdown('#### CO2 emissions are the primary driver of global climate change'),
-               pn.pane.PNG('https://raw.githubusercontent.com/sihlemsk/co2_dashboard/master/Climate-Change-Transparent.png', sizing_mode='scale_both'),
-               pn.pane.Markdown('## Settings'),
-               year_slider],
-    main=[pn.Row(pn.Column(yaxis_co2, 
-                           co2_plot.panel(width=700), margin=(0,25)), 
-                 co2_table.panel(width=500)), 
-          pn.Row(pn.Column(co2_vs_gdp_scatterplot.panel(width=600), margin=(0,25)), 
+    title='World CO2 Emission Dashboard',
+    sidebar=[pn.pane.Markdown('# CO2 Emissions and Climate Change'),
+             pn.pane.Markdown('#### CO2 emissions are the primary driver of global climate change'),
+             pn.pane.PNG('https://raw.githubusercontent.com/sihlemsk/co2_dashboard/master/Climate-Change-Transparent.png', sizing_mode='scale_both'),
+             pn.pane.Markdown('## Settings'),
+             year_slider],
+    main=[pn.Row(pn.Column(yaxis_co2,
+                           co2_plot.panel(width=700), margin=(0, 25)),
+                 co2_table.panel(width=500)),
+          pn.Row(pn.Column(co2_vs_gdp_scatterplot.panel(width=600), margin=(0, 25)),
                  pn.Column(yaxis_co2_source, co2_source_bar_plot.panel(width=600)))],
     accent_base_color="#88d8b0",
     header_background="#88d8b0",
 )
 
-pn.serve(template)
+template.show()
